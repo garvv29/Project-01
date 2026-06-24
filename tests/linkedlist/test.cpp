8452fdefbd02a9b4e3bf472878cf74bdb6c8c21e
garvv29 <garv.choure2023@ssipmt.com>
2026-06-24 11:33:51 +0530

    LinkedList Implemented

--- tests/linkedlist/test.cpp
+++ tests/linkedlist/test.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <SinglyList>
-
+using namespace std;
 int main()
 {
+    try{
     SinglyList<int> l;
     l.insert(0,10);
     l.insert(0,11);
     l.insert(1,12);
     l.insert(3,13);
     l.print();
-    l.clear();
+    std::cout<<l.size()<<endl;
+    l.remove(4);
     l.print();
+    }
+    catch(...){
+        cout<<"Error : "<<"out of bound"<<endl;
+    }
+
 }
